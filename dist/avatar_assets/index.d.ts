65d5635f416293d58e2df86512b961a1990da8b6
chenzhenxing.jensen <chenzhenxing.jensen@bytedance.com>
2021-12-16 11:44:37 +0800

    delete ignore file

--- dist/avatar_assets/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare module "*.svg" {
-  const value: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
-  export default value;
-}
-
-declare module "*.webp" {
-  const value: string;
-  export default value;
-}
